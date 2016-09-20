832de6ec092e141042583f70eb6194abcee9ce32
whereitat <erik.hallkvist@gmail.com>
2016-09-20 14:15:31 +0200

    Startup

---
+++ World Of Plebs/Main/model/Pleb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.model
+{
+    class Pleb
+    {
+        private string name;
+        private int level = 1;
+        private int attack;
+        private 
+    }
+}
